88241e109b4c2bc91d7a0deaca17a0b907ccbc88
bounty <mathiscucherat@gmail.com>
2024-11-29 20:03:22 +0100

    add crunchyroll scraping + start design popup

--- Ani-extension/foreground.js
+++ Ani-extension/foreground.js
@@ -9,6 +9,7 @@
 
 class anime {
   constructor() {
+      this.name = "";
       this.title = "";
       this.episode = "";
       this.link = "";
@@ -28,9 +29,47 @@ function voiranime(animeClass) {
   }
   animeClass.title = document.querySelector('h1').textContent;
 }
+//
 
-function crunchyroll(animeCLass) {
-    
+function crunchyroll(animeClass,host,callback) {
+  // Find the title of the anime
+  if (host == 'www.crunchyroll.com') {
+    // Trouver le conteneur à observer
+    let targetNode = document.getElementById('content'); 
+    // Configuration de l'observateur
+    let config = { childList: true, subtree: true };
+
+    // Callback pour gérer les mutations
+    let observerCallback = function(mutationsList, observer) {
+      for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+          let titleNode = document.querySelector('h1');
+          if (titleNode) {
+            const h1 = titleNode.textContent.split(' - ');
+            animeClass.episode = h1[0].substring(1);
+            animeClass.title = h1[1];
+            callback();
+            observer.disconnect();
+            break;
+          }
+        }
+      }
+    };
+
+    // Création de l'observateur
+    let observer = new MutationObserver(observerCallback);
+    observer.observe(targetNode, config);
+  }
+  else if (host == 'static.crunchyroll.com') {
+    let video = document.querySelector('video');
+    video.addEventListener('timeupdate', () => {
+      console.log(Math.floor(video.currentTime) );
+    });
+  }
+  else {
+    console.log('Host not supported');
+  }
+
 }
 
 
@@ -43,25 +82,14 @@ switch (location.hostname) {
     voiranime(anime);
     break;
 
-  case 'crunchyroll.com':
-    crunchyroll(anime);
+  case 'www.crunchyroll.com':
+  case 'static.crunchyroll.com':
+    crunchyroll(anime,location.hostname, () => {
+      // callback funct to save data
+    });
+
     break;
 
   default:
     break;
 }
-
-
-let video = document.querySelector('video');
-
-//console.log(video);
-
-video.addEventListener('play', () => {
-  
-});
-// video.duration;
-// video.currentTime;
-
-video.addEventListener('timeupdate', () => {
-  console.log(Math.floor(video.currentTime) );
-});
